beaa6e99dbb8fa8ddb0a4f28dd3def065cf0c557
AssassinG <hangq_zju@hotmail.com>
2019-03-25 09:01:22 +0800

    commit

--- src/main/java/cn/AssassinG/ScsyERP/OutStorage/core/biz/impl/OutStorageFormBizImpl.java
+++ src/main/java/cn/AssassinG/ScsyERP/OutStorage/core/biz/impl/OutStorageFormBizImpl.java
@@ -283,6 +283,7 @@ public class OutStorageFormBizImpl extends FormBizImpl<OutStorageForm> implement
         }
         product.setOutStorageForm(outStorageForm.getId());
         product.setStatus(ProductStatus.YCK);
+        System.out.println(outStorageForm);
         outStorageForm.getProducts().add(product.getId());
         this.update(outStorageForm);
         productServiceFacade.update(product);
